855b7b0527ad5a8a68583a10080c785838e89c73
Stella <stellaclough@gmail.com>
2025-09-16 11:10:59 +0200

    import van html inhoud

--- my-app/src/routes/+page.server.js
+++
@@ -1,6 +0,0 @@
-export async function load({URL}) {
-    const members= await fetch ('https://fdnd.directus.app/items/squad/?fields=persons.*.*&filter[cohort][_eq]=2526')
-    const membersData = await membersResponse.json();
-
-return {memers: membersData.data}
-}
--- my-app/src/routes/id/+page.server.js
+++
@@ -1,8 +0,0 @@
-export async function load({url, params}) {
-    console.log(params);
-
-    const member= await fetch ('https://fdnd.directus.app/items/person/'+ params.id')
-    const memberData = await memberResponse.json();
-
-return {member: membersData.data}
-}
